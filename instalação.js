0b67650eca62a2f499b8188bee8cc32ee2e8ec50
niko <68440743+Nikkwo@users.noreply.github.com>
2023-06-10 18:17:46 +0000

    Create instalação.js

---
+++ instalação.js
@@ -0,0 +1,23 @@
+const execa = require('execa');
+
+async function verificarDependencia(dependencia) {
+  try {
+    await execa.command(`node -e "require('${dependencia}')"`);
+    console.log(`A dependência '${dependencia}' está instalada.`);
+  } catch (error) {
+    console.log(`A dependência '${dependencia}' não está instalada.`);
+    await instalarDependencia(dependencia);
+  }
+}
+
+async function instalarDependencia(dependencia) {
+  try {
+    console.log(`Instalando a dependência '${dependencia}'...`);
+    await execa.command(`npm install ${dependencia}`);
+    console.log(`A dependência '${dependencia}' foi instalada com sucesso.`);
+  } catch (error) {
+    console.error(`Ocorreu um erro ao instalar a dependência '${dependencia}':`, error);
+  }
+}
+
+const dependencia = "#dependencia_aqui";
